8d04d864467e511c0e5e4890a34ade452c59ba8a
kotatama18 <tearth0619@gmaol.com>
2025-01-26 06:58:28 +0000

    8.レイアウトファイルの作成まで

--- src/App.tsx
+++ src/App.tsx
@@ -4,14 +4,17 @@ import {Route, BrowserRouter as Router, Routes} from 'react-router-dom';
 import Home from './pages/Home.tsx';
 import Report from './pages/Report.tsx';
 import NoMatch from './pages/NoMatch.tsx';
+import AppLayout from './components/layout/AppLayout.tsx';
 
 function App() {
   return (
    <Router>
     <Routes>
-      <Route path="/" element={<Home />}/>
+      <Route path="/" element={<AppLayout />}> 
+      <Route index element={<Home />}/>
       <Route path="/report" element={<Report />}/>
       <Route path="*" element={<NoMatch />}/>
+      </Route>
     </Routes>
    </Router>
   );

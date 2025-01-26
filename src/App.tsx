e4b46c3dc485a9af85e9b9e0c5ecdfcb1b531ff6
kotatama18 <tearth0619@gmaol.com>
2025-01-26 20:24:54 +0900

    10. レイアウトファイルの構造を確認しよう

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import './App.css';
-import {Route, BrowserRouter as Router, Routes} from 'react-router-dom';
+import { Route, BrowserRouter as Router, Routes } from 'react-router-dom';
 import Home from './pages/Home.tsx';
 import Report from './pages/Report.tsx';
 import NoMatch from './pages/NoMatch.tsx';
@@ -8,15 +8,15 @@ import AppLayout from './components/layout/AppLayout.tsx';
 
 function App() {
   return (
-   <Router>
-    <Routes>
-      <Route path="/" element={<AppLayout />}> 
-      <Route index element={<Home />}/>
-      <Route path="/report" element={<Report />}/>
-      <Route path="*" element={<NoMatch />}/>
-      </Route>
-    </Routes>
-   </Router>
+    <Router>
+        <Routes>
+          <Route path="/" element={<AppLayout />}> 
+            <Route index element={<Home />}/>
+            <Route path="/report" element={<Report />}/>
+            <Route path="*" element={<NoMatch />}/>
+          </Route>
+        </Routes>
+    </Router>
   );
 }
 
